b19901f1d6cbb3cd5861bd3fefa4901b9907cae3
nitiancheng <1343810646@qq.com>
2020-06-24 17:04:18 +0800

    更新了文件

--- code.js
+++ code.js
@@ -7,7 +7,17 @@ glob模式
 [abc]匹配任何一个列在方括号中的字符
 问号？只匹配一个任意字符
 
+git log
+
+git reflog --oneline 一行显示，简洁一点
+
 回退到某个版本：git reset --hard xxx
+
+
+git init
+git add / git add .
+git status / git status -s
+git commit -m '描述信息'
 
 
 */
